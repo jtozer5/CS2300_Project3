21f8ae3d96d88e63085c0a73747af56c84bbce8f
willsonnathan <55596971+willsonnathan@users.noreply.github.com>
2019-11-03 15:35:51 -0700

    Update project3.m

--- project3.m
+++ project3.m
@@ -18,7 +18,7 @@ for c = 1:n
 end
     
 sys_prod = [sys prod]; 
-
+disp("The system matrix:");
 disp(sys_prod)
 
 %Gets the row of the highest first column value.
@@ -26,7 +26,6 @@ disp(sys_prod)
 
 %Moves the row with the largest absolute value to the top.
 sys_prod([1,row],:) = sys_prod([row, 1],:);
-disp(sys_prod);
 
 %Zeros the element at (2,1) by multiplying the first row by 6.3, the second
 %row by -8.1 and adding them to create the new second row.
@@ -35,7 +34,6 @@ secondRow = -sys_prod(1,1);
 for j = 1:n + 1
     sys_prod(2,j)=(firstRow*(sys_prod(1,j))+secondRow*sys_prod(2,j));
 end
-disp(sys_prod);
 
 %Multiplies the first row by -7.5 and the third row by -8.1 and add to
 %create the new third row.
@@ -44,12 +42,24 @@ thirdRow = -sys_prod(1,1);
 for k = 1:n+1
     sys_prod(3,k) = firstRow*(sys_prod(1,k))+thirdRow*sys_prod(3,k);
 end
+disp("Column 1:")
 disp(sys_prod);
 
 %Multiplies row 3 by -50.85, row 2 by -45.3 and add to form row
 secondRow = -sys_prod(3,2);
 thirdRow = sys_prod(2,2);
 for l = 1:n+1
-    sys_prod(3,l) = secondRow*(sys_prod(2,l))+thirdRow*sys_prod(3,l);
+    sys_prod(3,l) = -(secondRow*(sys_prod(2,l))+thirdRow*sys_prod(3,l));
 end
+disp("Column 2:")
 disp(sys_prod);
+
+%Now that it is in upper triangular form we can solve for the three values.
+x_3 = sys_prod(3,4)/sys_prod(3,3);
+x_2=(sys_prod(2,4)-sys_prod(2,3)*x_3)/sys_prod(2,2);
+x_1=(sys_prod(1,4) - sys_prod(1,3)*x_2 - sys_prod(1,2)*x_3)/sys_prod(1,1);
+
+disp("Solution vector:");
+disp(x_1);
+disp(x_2);
+disp(x_3);
