679fedaccf7e054c4b8b9ca11c49e16692a1fa31
willsonnathan <55596971+willsonnathan@users.noreply.github.com>
2019-11-03 14:36:02 -0700

    Update project3.m

--- project3.m
+++ project3.m
@@ -27,3 +27,21 @@ disp(sys_prod)
 %Moves the row with the largest absolute value to the top.
 sys_prod([1,row],:) = sys_prod([row, 1],:);
 disp(sys_prod);
+
+%Zeros the element at (2,1) by multiplying the first row by 6.3, the second
+%row by -8.1 and adding them to create the new second row.
+firstRow = sys_prod(2,1);
+secondRow = -sys_prod(1,1);
+for j = 1:n + 1
+    sys_prod(2,j)=(firstRow*(sys_prod(1,j))+secondRow*sys_prod(2,j));
+end
+disp(sys_prod);
+
+%Multiplies the first row by -7.5 and the third row by -8.1 and add to
+%create the new third row.
+firstRow = sys_prod(3,1);
+thirdRow = -sys_prod(1,1);
+for k = 1:n+1
+    sys_prod(3,k) = firstRow*(sys_prod(1,k))+thirdRow*sys_prod(3,k);
+end
+disp(sys_prod);
