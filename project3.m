b3cf2f5f9745c32578bcedd498346fb33e7f4082
Josh Tozer <joshiantozer@gmail.com>
2019-11-07 13:25:52 -0700

    clean up

--- project3.m
+++ project3.m
@@ -1,13 +1,20 @@
-% Project 3
+%{
+Nathan Willson & Josh Tozer
+CS 2300 002
+Project 2
+11/7/2019
+%}
 
-data = dlmread('sysmat1.txt');
-prod_data = dlmread('prodvec1.txt');
+% Read data files
+data = dlmread('sysmat2.txt');
+prod_data = dlmread('prodvec2.txt');
 n = data(1);
 prod = prod_data(2:n+1);
 prod = (prod)';
 
 sys = zeros(n);
 
+% Put data into a matrix
 i = 2;
 for c = 1:n
     for r = 1:n
@@ -15,43 +22,46 @@ for c = 1:n
         i = i + 1;
     end
 end
-    
+
+% Concat system matrix and product matrix
 sys_prod = [sys prod]; 
 disp("The system matrix:");
 disp(sys_prod)
 
 %Gets the row of the highest first column value.
-[maxNumber, row] = max(sys_prod(:,1));
+[maxNumber, row] = max(sys_prod(:, 1));
 
 %Moves the row with the largest absolute value to the top.
-sys_prod([1,row],:) = sys_prod([row, 1],:);
+sys_prod([1,row], :) = sys_prod([row, 1], :);
 
 z = 2;
-for j = 1:n-1
+for j = 1:n - 1
     
     disp(sys_prod);
     for l = z:n
-        firstRow = sys_prod(l,j);
-        secondRow = -sys_prod(j,j);
-        for k = 1:n+1
-            sys_prod(l,k)=(firstRow*(sys_prod(j,k))+secondRow*sys_prod(l,k));
+        first_row = sys_prod(l, j);
+        second_row = -sys_prod(j, j);
+        for k = 1:n + 1
+            sys_prod(l, k) = first_row * sys_prod(j, k) + second_row * sys_prod(l, k);
         end
     end
     
     disp("Column "+ j +":")
     disp(sys_prod);
-    if(z<n)
-        if(abs(sys_prod(z,z))<abs(sys_prod(z+1,z)))
-            temp = sys_prod(z + 1,:);
-            sys_prod(z+1,:) = sys_prod(z,:);
-            sys_prod(z, :)=temp;
+    if(z < n)
+        if abs(sys_prod(z,z)) < abs(sys_prod(z+1,z))
+            temp = sys_prod(z + 1, :);
+            sys_prod(z+1, :) = sys_prod(z, :);
+            sys_prod(z, :) = temp;
         end
     end
     
-    z = z+1;
+    z = z + 1;
 end
 
+disp("System vector:");
+disp(sys_prod);
 %Now that it is in upper triangular form we can solve for the three values.
 disp("Solution vector:");
-x=inv(sys)*prod;
-disp(x);
+solution = sys\prod;
+disp(solution);
